5ec62c71c18c86b28e5564bdc70e38753ac1fa72
nafamubarokhusni <nafamubarokhusni@gmail.com>
2025-05-03 18:59:14 +0700

    Remove unused imports from eval.py for cleaner code

--- evaluation/eval.py
+++ evaluation/eval.py
@@ -2,9 +2,6 @@ import os
 import re
 import numpy as np
 import tensorflow as tf
-from tqdm import tqdm
-from pycocoevalcap.cider.cider import Cider
-from VideoCaptioning.data import *
 
 class EvalMetrics():
     def __init__(self, model, vectorization, SEQ_LENGTH, valid_dataset, valid_data, FRAMES_STORAGE_PATH, SPATIAL_SIZE=224, max_frames=20):
