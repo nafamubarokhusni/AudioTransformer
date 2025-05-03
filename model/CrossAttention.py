7e8fac5e8b849b4f8b1d3fab4fa653ec99857c92
nafamubarokhusni <nafamubarokhusni@gmail.com>
2025-05-03 19:18:39 +0700

    Add EMBED_DIM constant to CrossAttention.py for clarity

--- model/CrossAttention.py
+++ model/CrossAttention.py
@@ -4,6 +4,7 @@ import numpy as np
 
 NUM_FEATURE = 8  # Replace with the correct value
 SEQ_LENGTH = 512  # Replace with the correct value
+EMBED_DIM = 512  # Replace with the correct value
 
 class PositionalEncoding(tf.keras.layers.Layer):
     def __init__(self, sequence_length, embed_dim, trainable_embed=False, **kwargs):
